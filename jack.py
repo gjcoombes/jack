1d8b66cd919449c8a974cadd5836295ff7bcd22b
Gavin Coombes <gjcoombes@gmail.com>
2013-07-14 14:54:25 +0800

    added new_fetch_record and some imports
    Think I will probably make the fetc record a class soon.

--- jack.py
+++ jack.py
@@ -7,20 +7,52 @@ Module: jack.py
 Description: The interactive commandline client
 
 This is really a learning platform for interactive tools, but as tools develop
-they will spun off or incorporated further.
+they will spun off, or incorporated further.
 
 The first tool to be included is the check_winfates_results, with cross-machine
-and file movement extensions.
+search and file movement extensions.
 
 """
 from __future__ import print_function
 
 import sys
+import os
+import os.path as osp
+from pprint import pprint
+from fnmatch import filter as fnfilter
 
 from cmd2 import Cmd
 ### Constants
 
 ### Classes
+
+def new_fetch_record():
+    """Return a default fetch record (dict) for filling in
+
+    type: fetch - from Jack perhaps
+    header: Contains everything needed to generate the body
+        paths: List of directories to search
+        patterns: List of patterns to apply
+        match_type: glob or regex
+        clobber: Overwrite files? default: True
+        cleanup: Delete fiels after they are moved? default - False for the moment
+    body: The payload to be executed or fetched
+        dest_dir: The directory to move files to
+        files:
+    """
+    return {"type": "fetch",
+            "header": {
+                "paths": [],
+                "patterns": [],
+                "match_type": "glob",
+                "clobber": True,
+                "cleanup": False,
+                },
+            "body": {
+                "files": [],
+                },
+            }
+
 class Jack(Cmd):
     """Commandline interpreter to access various tools
 
